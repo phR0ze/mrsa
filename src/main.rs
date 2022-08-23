5f296acca1ca2a7b59922e93a140e8456a6f5eee
phR0ze <prc451@gmail.com>
2022-08-23 17:10:47 -0600

    0.1.10: Android runnable macroquad and egui

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,33 @@ use mrsa_core::prelude::*;
 fn window_conf() -> Conf {
     Conf {
         window_title: "Text Image example".to_string(),
-        window_width: 1024,
-        window_height: 768,
+        fullscreen: false,
         high_dpi: true,
-        window_resizable: false,
         ..Default::default()
     }
 }
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let mut img = TextImage::new(screen_width() as u32, screen_height() as u32);
-    img.load_file("core/examples/assets/example.txt").await;
+    //let mut img = TextImage::new(screen_width() as u32, screen_height() as u32);
+    //img.load_file("core/examples/assets/example.txt").await;
 
     loop {
         clear_background(WHITE);
 
-        img.draw_center();
+        // img.draw_center();
+        let (x, y) = (screen_width() / 2., screen_height() / 2.);
+        draw_text("IT WORKS!", x, y, 50.0, DARKGRAY);
+
+        egui_macroquad::ui(|egui_ctx| {
+            egui::Window::new("egui ❤ macroquad").show(egui_ctx, |ui| {
+                ui.label("Test");
+            });
+        });
+
+        // Draw things before egui
+        egui_macroquad::draw();
+        // Draw things after egui
 
         next_frame().await
     }
